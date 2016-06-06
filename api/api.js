df84c0b099fa651fa8bdba21cc2fadf5695ce342
Tom Coleman <tom@thesnail.org>
2016-06-06 16:55:33 +1000

    Brought apollo server starter kit into example app

--- api/api.js
+++ api/api.js
@@ -1,91 +1,24 @@
 import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
+import { apolloServer } from 'apollo-server';
+import Schema from './data/schema';
+import Mocks from './data/mocks';
+
 import config from '../src/config';
-import * as actions from './actions/index';
-import {mapUrl} from 'utils/url.js';
-import PrettyError from 'pretty-error';
-import http from 'http';
-import SocketIo from 'socket.io';
+const ENDPOINT = '/graphql';
 
-const pretty = new PrettyError();
-const app = express();
-
-const server = new http.Server(app);
-
-const io = new SocketIo(server);
-io.path('/ws');
-
-app.use(session({
-  secret: 'react and redux rule!!!!',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 60000 }
+const graphQLServer = express();
+graphQLServer.use(ENDPOINT, apolloServer({
+  graphiql: true,
+  pretty: true,
+  schema: Schema,
+  mocks: Mocks,
 }));
-app.use(bodyParser.json());
-
-
-app.use((req, res) => {
-  const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
-
-  const {action, params} = mapUrl(actions, splittedUrlPath);
-
-  if (action) {
-    action(req, params)
-      .then((result) => {
-        if (result instanceof Function) {
-          result(res);
-        } else {
-          res.json(result);
-        }
-      }, (reason) => {
-        if (reason && reason.redirect) {
-          res.redirect(reason.redirect);
-        } else {
-          console.error('API ERROR:', pretty.render(reason));
-          res.status(reason.status || 500).json(reason);
-        }
-      });
-  } else {
-    res.status(404).end('NOT FOUND');
-  }
-});
-
-
-const bufferSize = 100;
-const messageBuffer = new Array(bufferSize);
-let messageIndex = 0;
 
 if (config.apiPort) {
-  const runnable = app.listen(config.apiPort, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.info('----\n==> 🌎  API is running on port %s', config.apiPort);
-    console.info('==> 💻  Send requests to http://%s:%s', config.apiHost, config.apiPort);
+  graphQLServer.listen(config.apiPort, () => {
+    console.info('----\n==> 🌎  GraphQL Server is running on port %s', config.apiPort);
+    console.info('==> 💻  Send queries to http://%s:%s%s', config.apiHost, config.apiPort, ENDPOINT);
   });
-
-  io.on('connection', (socket) => {
-    socket.emit('news', {msg: `'Hello World!' from server`});
-
-    socket.on('history', () => {
-      for (let index = 0; index < bufferSize; index++) {
-        const msgNo = (messageIndex + index) % bufferSize;
-        const msg = messageBuffer[msgNo];
-        if (msg) {
-          socket.emit('msg', msg);
-        }
-      }
-    });
-
-    socket.on('msg', (data) => {
-      data.id = messageIndex;
-      messageBuffer[messageIndex % bufferSize] = data;
-      messageIndex++;
-      io.emit('msg', data);
-    });
-  });
-  io.listen(runnable);
 } else {
   console.error('==>     ERROR: No PORT environment variable has been specified');
 }
